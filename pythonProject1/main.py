181a04091b41f67d6e0f83cc2ba5c2e2e799ae87
clilexie <154313768+clyle342@users.noreply.github.com>
2023-12-21 13:58:37 +0300

    Py files

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -1,16 +1,66 @@
-# This is a sample Python script.
+from flask import Flask, render_template, request
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+
+app = Flask(__name__)
+
+# Load the pre-trained model and scaler
+model = LogisticRegression(max_iter=1000)
+scaler = StandardScaler()
+
+# Load the dataset (replace with the actual path to your dataset)
+data = pd.read_csv("C:/Users/PC/Diabetes/dataset.csv")
+
+# Identify independent variables (features) and the target variable (Outcome)
+X = data.drop("Outcome", axis=1)  # Features
+y = data["Outcome"]  # Target variable
+
+# Standardize the features
+scaler = StandardScaler(with_mean=False)
+X_scaled = scaler.fit_transform(X)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Train the model
+model.fit(X_scaled, y)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        values = [
+            float(request.form['pregnancies']),
+            float(request.form['glucose']),
+            float(request.form['blood_pressure']),
+            float(request.form['skin_thickness']),
+            float(request.form['insulin']),
+            float(request.form['bmi']),
+            float(request.form['dpf']),
+            float(request.form['age'])
+        ]
+
+        # Standardize input features
+        values_scaled = scaler.transform([values])
+
+        # Debug prints for form data and prediction
+        print("Form Data:", values)
+        print("Scaled Input:", values_scaled)
+
+        # Make prediction
+        prediction = model.predict(values_scaled)
+
+        # Debug print for prediction result
+        print("Prediction:", prediction)
+
+        return render_template('result.html', prediction=prediction[0])
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run(debug=True)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+print("Form Data:", request.form)
